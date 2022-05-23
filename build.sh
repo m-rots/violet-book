752ad8e065d725b664a2e7f286dad398dd06b532
Storm Timmermans <stormtimmermans@icloud.com>
2022-05-23 14:48:07 +0200

    ci: use custom mdbook binary

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-MDBOOK_URL="https://github.com/rust-lang/mdBook/releases/download/v0.4.18/mdbook-v0.4.18-x86_64-unknown-linux-gnu.tar.gz"
-
-curl -L --output "mdbook.tar.gz" $MDBOOK_URL
-tar -xvzf mdbook.tar.gz
 chmod +x mdbook
 
 ./mdbook build
